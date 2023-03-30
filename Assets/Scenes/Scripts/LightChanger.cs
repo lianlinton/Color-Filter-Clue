56a1b63d5c36f3da70da0fe7cf991993807f18b5
lianlinton <lian.linton@gmail.com>
2023-03-30 00:22:15 -0700

    Added 3 levels

--- Assets/Scenes/Scripts/LightChanger.cs
+++ Assets/Scenes/Scripts/LightChanger.cs
@@ -8,13 +8,21 @@ public class LightChanger : MonoBehaviour
 {
     public Light pointLight = null;
     public InputAction action;
-    public Texture showLevel1;
-    public Texture showLevel2;
-    public Texture showLevel3;
-    public Texture showLevel4;
-    public Texture hide;
+    public Texture hatLevel;
+    public Texture imageLevel;
+    public Texture bookLevel;
+    public Texture hideLevel;
+    public Color hatWallColor;
+    public Color imageWallColor;
+    public Color bookWallColor;
     private Renderer renderer;
     public GameObject plane;
+    public GameObject wall1;
+    public GameObject wall2;
+    public GameObject wall3;
+    public GameObject wall4;
+    public GameObject bottomWall;
+    public GameObject topWall;
     public int level;
 
     private void OnEnable()
@@ -30,7 +38,7 @@ public class LightChanger : MonoBehaviour
     private void Start()
     {
         renderer = plane.GetComponent<Renderer>();
-        renderer.material.mainTexture = hide;
+        renderer.material.mainTexture = hatLevel;
     }
 
     void Update()
@@ -39,41 +47,47 @@ public class LightChanger : MonoBehaviour
         Light l = pointLight.GetComponent<Light>();
         Color lightColor;
         Texture showText;
+        Color showColor;
         switch (level)
         {
-            case 1:
-                lightColor = Color.blue;
-                showText = showLevel2;
-                break;
             case 2:
-                lightColor = new Color(0.3f, 0.4f, 0.6f, 0.3f);
-                showText = showLevel3;
+                lightColor = imageWallColor;
+                showText = imageLevel;
+                showColor = imageWallColor;
                 break;
             case 3:
-                lightColor = Color.magenta;
-                showText = showLevel4;
+                lightColor = bookWallColor;
+                showText = bookLevel;
+                showColor = bookWallColor;
                 break;
             default:
-                lightColor = Color.red;
-                showText = showLevel1;
+                lightColor = hatWallColor;
+                showText = hatLevel;
+                showColor = hatWallColor;
                 break;
         }
 
         if (action.IsPressed())
         {
-
-            if (!l.enabled)
-            {
+            //if (!l.enabled)
+            //{
                 l.color = lightColor;
                 l.enabled = true;
                 renderer.material.mainTexture = showText;
+                renderer.material.mainTexture = showText;
+                wall1.GetComponent<Renderer>().material.color = showColor;
+                wall2.GetComponent<Renderer>().material.color = showColor;
+                wall3.GetComponent<Renderer>().material.color = showColor;
+                wall4.GetComponent<Renderer>().material.color = showColor;
+                topWall.GetComponent<Renderer>().material.color = showColor;
+                bottomWall.GetComponent<Renderer>().material.color = showColor;
 
-            }
+            /*}
             else
             {
                 l.enabled = false;
-                renderer.material.mainTexture = hide;
-            }
+                renderer.material.mainTexture = hideLevel;
+            }*/
         }
     }
 }
